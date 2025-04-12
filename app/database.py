3e6a08adef83481a0282aaf7f52310994cfe8a6e
@aleksandr_turshiev <240720008@mail.ru>
2025-04-13 02:59:01 +0700

    Add database initialization

---
+++ app/database.py
@@ -0,0 +1,14 @@
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+Base = declarative_base()
